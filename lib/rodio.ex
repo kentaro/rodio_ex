c72397f6beebf7cf28255c4b92d9c446a3b3a697
Kentaro Kuribayashi <kentarok@gmail.com>
2024-01-18 19:45:58 +0900

    mix new rodio

---
+++ lib/rodio.ex
@@ -0,0 +1,18 @@
+defmodule Rodio do
+  @moduledoc """
+  Documentation for `Rodio`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rodio.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rodio_test.exs
@@ -0,0 +1,8 @@
+defmodule RodioTest do
+  use ExUnit.Case
+  doctest Rodio
+
+  test "greets the world" do
+    assert Rodio.hello() == :world
+  end
+end
